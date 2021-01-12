d0c3f09108c1d80dd572db70094e5570130e001f
Deepanshu Goel <59753906+symaticvisuals@users.noreply.github.com>
2021-01-12 14:11:29 +0530

    project-layout

---
+++ src/main/java/com/tems/ResumeBuilder/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.tems.ResumeBuilder;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ResumeBuilderApplication.class);
+	}
+
+}
---
+++ src/test/java/com/tems/ResumeBuilder/ResumeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tems.ResumeBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
